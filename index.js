9803f7c98661519d35b5d0a257bc4094cdd0a100
ClemZ08 <clement.rfd@gmail.com>
2023-09-20 16:03:59 +0200

    new4

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
-function time() {
+function updateTime() {
   const now = new Date();
-  const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
-  const formattedDateTime = now.toLocaleDateString('fr-FR', options);
+  
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
 
-  document.getElementById('time').textContent = formattedDateTime;
+  const timeString = hours + ':' + minutes;
+  document.getElementById('time').textContent = timeString;
 }
-time();
-setInterval(time, 1000);
 
+updateTime();
 
-document.getElementById('showSlideBtn').addEventListener('click', function() {
-  const slide = document.getElementById('slide');
-  slide.classList.toggle('visible');
+setInterval(updateTime, 60 * 1000);
+
+function updateDate() {
+  const now = new Date();
+  
+  const day = String(now.getDate()).padStart(2, '0');
+  const month = String(now.getMonth() + 1).padStart(2, '0');  // Les mois sont de 0 (janvier) à 11 (décembre) dans JavaScript.
+  const year = now.getFullYear();
+
+  const dateString = day + '/' + month + '/' + year;
+  document.getElementById('date').textContent = dateString;
+}
+
+updateDate();
+
+const searchBar = document.querySelector('.input');
+const suggestedArticles = document.querySelector('.suggested-articles');
+
+// Affichez la liste déroulante lorsque la barre de recherche est cliquée
+searchBar.addEventListener('focus', function() {
+    suggestedArticles.style.display = 'block';
 });
+
+// Cachez la liste déroulante lorsque l'utilisateur clique ailleurs sur la page
+document.addEventListener('click', function(event) {
+    if (!searchBar.contains(event.target) && !suggestedArticles.contains(event.target)) {
+        suggestedArticles.style.display = 'none';
+    }
+});
+
