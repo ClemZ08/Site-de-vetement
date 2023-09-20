ee53709705394fc54560faaf295b5ae263a71eb7
ClemZ08 <clement.rfd@gmail.com>
2023-09-20 17:17:11 +0200

    new5

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function updateDate() {
   const now = new Date();
   
   const day = String(now.getDate()).padStart(2, '0');
-  const month = String(now.getMonth() + 1).padStart(2, '0');  // Les mois sont de 0 (janvier) à 11 (décembre) dans JavaScript.
+  const month = String(now.getMonth() + 1).padStart(2, '0');
   const year = now.getFullYear();
 
   const dateString = day + '/' + month + '/' + year;
@@ -28,12 +28,10 @@ updateDate();
 const searchBar = document.querySelector('.input');
 const suggestedArticles = document.querySelector('.suggested-articles');
 
-// Affichez la liste déroulante lorsque la barre de recherche est cliquée
 searchBar.addEventListener('focus', function() {
     suggestedArticles.style.display = 'block';
 });
 
-// Cachez la liste déroulante lorsque l'utilisateur clique ailleurs sur la page
 document.addEventListener('click', function(event) {
     if (!searchBar.contains(event.target) && !suggestedArticles.contains(event.target)) {
         suggestedArticles.style.display = 'none';
